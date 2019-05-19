1ecb80e3e80bd347ba967a677974b1a330238231
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-05-19 10:45:46 +0300

    Part4 - Q2 (Rational)

---
+++ Part4_Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1505_Part4_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational r1 = new Rational(12, 5);
+            Rational r2 = new Rational(11, 6);
+            Console.WriteLine($"r1 {r1}");
+            Console.WriteLine($"r2 {r2}");
+
+            Console.WriteLine($"Greater then is: {r1.greaterThan(r2)}");
+            Console.WriteLine($"Equals is: {r1.equals(r2)}");            
+            Console.WriteLine($"rSum is: {r1.plus(r2)}");
+            Console.WriteLine($"Minus is: {r1.minus(r2)}");
+            Console.WriteLine($"Multiply then is: {r1.multiply(r2)}");
+            Console.WriteLine($"r1.GetNumerator then is: {r1.GetNumerator()}");
+            Console.WriteLine($"r2.getDenominator then is: {r2.getDenominator()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Part4_Q2/Rational.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1505_Part4_Q2
+{
+    class Rational
+    {
+        int mone;
+        int mechane;
+        double rationalNumber;
+        public Rational(int mone, int mechane)
+        {
+            this.mone = mone;
+            this.mechane = mechane;
+
+            if ((mone % 1 == 0) && (mechane > 0))
+            {
+                rationalNumber = ((double)(mone / mechane));
+            }
+            else
+                rationalNumber = 0;
+        }
+
+        public bool greaterThan(Rational rationalNum)
+        {
+            return ((this.mone * rationalNum.mechane) > (this.mechane * rationalNum.mone));
+        }
+
+        public bool equals (Rational rationalNum)
+        {
+            return ((this.mone * rationalNum.mechane) == (this.mechane * rationalNum.mone));
+        }
+
+        public Rational plus(Rational rNum)
+        {
+            return (new Rational(((this.mone * rNum.mechane) + (this.mechane * rNum.mone)) , (this.mechane * rNum.mechane)));
+        }
+
+        public Rational minus(Rational rNum)
+        {
+            return (new Rational(((this.mone * rNum.mechane) - (this.mechane * rNum.mone)), (this.mechane * rNum.mechane)));
+        }
+
+        public Rational multiply(Rational rNum)
+        {
+            return (new Rational(((this.mone * rNum.mone)), (this.mechane * rNum.mechane)));
+        }
+
+        public int GetNumerator()
+        {
+            return this.mone;
+        }
+
+        public int getDenominator()
+        {
+            return this.mechane;
+        }
+
+        public override string ToString()
+        {
+            return $"Mone is {mone}, Mechane is {mechane} The number is: {mone}/{mechane}.";
+        }
+    }
+}
